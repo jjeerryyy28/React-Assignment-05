aab494c092ec3b1eb3a9b4162aad833a697e98a3
jjeerryyy28 <prajwalnikhar1@gmail.com>
2023-05-31 00:48:05 +0530

    React Assignment

---
+++ assignment-05/src/Components/MainSection/MainSection.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const MainSection = () => {
+  return (
+    <div className="main-section">
+      <div className="background-image">
+        <h1>Home</h1>
+      </div>
+    </div>
+  );
+};
+
+export default MainSection;
---
+++ assignment-05/src/Components/Nav/Nav.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Nav = () => {
+  return (
+    <nav>
+      <div className="logo">Logo</div>
+      <div className="links">
+        <a href="#" id='home'>Home</a>
+        <a href="#">About</a>
+        <a href="#">Menu</a>
+        <a href="#">Order</a>
+        <a href="#">Contact Us</a>
+      </div>
+    </nav>
+  );
+};
+
+export default Nav;
