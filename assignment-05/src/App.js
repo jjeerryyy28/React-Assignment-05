8ff16db4ca3e48b3efe56073746a2d8023a03bc4
jjeerryyy28 <prajwalnikhar1@gmail.com>
2023-05-31 00:48:05 +0530

    React Assignment

---
+++ assignment-05/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import Nav from './Components/Nav/Nav';
+import MainSection from './Components/MainSection/MainSection'
+import Footer from './Components/Footer/Footer'
+
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+      <MainSection />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
